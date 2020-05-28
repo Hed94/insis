24e8735f8b3104c794e4b01f5857f82cffb5932b
Hed94 <Hed94@seznam.cz>
2020-05-28 12:22:15 +0200

    Vytvoření přihlašovací obrazovky

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../zdroje/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../zdroje/prihlasovaciObrazovka.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
